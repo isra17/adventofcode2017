608575da225929b91448f3be7582a582462cef5d
isra17 <isra017@gmail.com>
2017-12-04 11:04:03 -0500

    Completed day3

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,4 +1,5 @@
-fn solve1(n: i64) -> i64 {
+
+fn coord_for(n: i64) -> (i64, i64) {
     let mut base = (n as f64).sqrt().ceil() as i64;
 
     if base % 2 == 0 {
@@ -6,71 +7,77 @@ fn solve1(n: i64) -> i64 {
     }
 
     if base == 1 {
-        return 0;
+        return (0, 0);
     }
 
     let prev_square = (base - 2) * (base - 2);
     let side = (n - prev_square) / (base - 1);
     let center = prev_square + base / 2 + (side * (base - 1));
-    (n - center).abs() + (base - 1) / 2
-}
+    let a = n - center;
+    let b = (base - 1) / 2;
 
-// struct Spiral {
-// data: Vec<i64>,
-// data_base: i64,
-// cursor_x: i64,
-// cursor_y: i64,
-// }
-//
-// impl Spiral {
-// fn new(size: usize) -> Spiral {
-// let spiral = Spiral {
-// data: Vec::with_capacity(size * size),
-// data_base: size,
-// cursor_x: 0,
-// cursor_y: 0,
-// };
-// spiral.data.resize(size * size, 0);
-// spiral
-// }
-//
-// fn push(self, value: i64) {}
-// }
-//
-
-struct SpiralSum {
-    cache: Vec<i64>,
-}
-
-impl SpiralSum {
-    fn new() -> SpiralSum {
-        SpiralSum { cache: Vec::new() }
+    match side {
+        0 | 4 => (b, a),
+        1 => (-a, b),
+        2 => (-b, -a),
+        3 => (a, -b),
+        _ => unreachable!(),
     }
 
-    fn get_value(&self, n: i64) -> i64 {
-        if n == 1 {
-            return 1;
-        } else if n <= 0 {
-            return 0;
+}
+fn solve1(n: i64) -> i64 {
+    let (x, y) = coord_for(n);
+    x.abs() + y.abs()
+}
+
+struct Spiral {
+    data: Vec<i64>,
+    size: usize,
+    cursor: usize,
+}
+
+static DIRECTIONS: &'static [&'static [i64]] = &[&[-1, -1], &[0, -1], &[1, -1], &[-1, 0], &[0, 0],
+                                                 &[1, 0], &[-1, 1], &[0, 1], &[1, 1]];
+
+impl Spiral {
+    fn new(size: usize) -> Spiral {
+        let mut spiral = Spiral {
+            data: Vec::with_capacity(size * size),
+            size: size,
+            cursor: 1,
+        };
+        spiral.data.resize(size * size, 0);
+        let center = size / 2;
+        spiral.data[center + center * size] = 1;
+        spiral
+    }
+
+    fn next(&mut self) -> Option<i64> {
+        if self.cursor >= self.size * self.size {
+            return None;
         }
-        let mut base = (n as f64).sqrt().ceil() as i64;
-        if base % 2 == 0 {
-            base = base + 1;
-        }
-        let prev_square = (base - 2) * (base - 2);
-        let side = (n - prev_square) / (base - 1);
-        0
+
+        let (x, y) = coord_for(self.cursor as i64);
+        self.cursor += 1;
+
+        let center = self.size as i64 / 2;
+        let value = DIRECTIONS.iter()
+            .map(|d| {
+                self.data[(d[0] + center + x) as usize + (d[1] + center + y) as usize * self.size]
+            })
+            .sum();
+        self.data[(center + x) as usize + (center + y) as usize * self.size] = value;
+        Some(value)
     }
 }
 
 fn solve2(n: i64) -> i64 {
-    let mut i = 1;
-    let spiral = SpiralSum::new();
+    let mut spiral = Spiral::new(11);
     loop {
-        if spiral.get_value(i) > n {
-            return spiral.get_value(i);
+        let v = spiral.next().unwrap() as i64;
+        if v > n {
+            return v;
         }
-        i += 1;
     }
 
 }
@@ -84,6 +91,21 @@ fn main() {
 mod tests {
     use solve1;
     use solve2;
+    use coord_for;
+    use Spiral;
+
+    #[test]
+    fn test_coord() {
+        assert_eq!(coord_for(9), (1, -1));
+        assert_eq!(coord_for(12), (2, 1));
+        assert_eq!(coord_for(14), (1, 2));
+        assert_eq!(coord_for(20), (-2, -1));
+        assert_eq!(coord_for(21), (-2, -2));
+        assert_eq!(coord_for(22), (-1, -2));
+        assert_eq!(coord_for(23), (0, -2));
+        assert_eq!(coord_for(25), (2, -2));
+    }
+
     #[test]
     fn test_solve1() {
         assert_eq!(solve1(1), 0);
@@ -92,16 +114,27 @@ mod tests {
         assert_eq!(solve1(1024), 31);
     }
 
-    // #[test]
-    // fn test_solve2() {
-    // assert_eq!(solve2(0), 1);
-    // assert_eq!(solve2(1), 2);
-    // assert_eq!(solve2(2), 4);
-    // assert_eq!(solve2(4), 5);
-    // assert_eq!(solve2(5), 10);
-    // assert_eq!(solve2(10), 11);
-    // assert_eq!(solve2(11), 23);
-    // assert_eq!(solve2(362), 747);
-    // }
-    //
+    #[test]
+    fn test_spiral() {
+        let mut spiral = Spiral::new(11);
+        assert_eq!(spiral.next(), Some(1));
+        assert_eq!(spiral.next(), Some(1));
+        assert_eq!(spiral.next(), Some(2));
+        assert_eq!(spiral.next(), Some(4));
+        assert_eq!(spiral.next(), Some(5));
+        assert_eq!(spiral.next(), Some(10));
+        assert_eq!(spiral.next(), Some(11));
+        assert_eq!(spiral.next(), Some(23));
+    }
+
+    #[test]
+    fn test_solve2() {
+        assert_eq!(solve2(5), 10);
+        assert_eq!(solve2(11), 23);
+        assert_eq!(solve2(23), 25);
+        assert_eq!(solve2(25), 26);
+        assert_eq!(solve2(59), 122);
+        assert_eq!(solve2(147), 304);
+        assert_eq!(solve2(747), 806);
+    }
 }
